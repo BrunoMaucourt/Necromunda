2844a515435de9e13a0bcb80f9aa880f148e9e79
Bruno <mct.bruno@gmail.com>
2024-05-24 20:08:54 +0200

    Update of territories CRUD - avoid others users to modify territories

--- src/Controller/Admin/TerritoriesCrudController.php
+++ src/Controller/Admin/TerritoriesCrudController.php
@@ -6,13 +6,22 @@ use App\EasyAdmin\TerritoriesField;
 use App\Entity\Territories;
 use App\Enum\TerritoriesEnum;
 use Doctrine\ORM\EntityRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class TerritoriesCrudController extends AbstractCrudController
 {
+    private $security;
+
+    public function __construct(Security $security){
+        $this->security = $security;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Territories::class;
@@ -21,7 +30,7 @@ class TerritoriesCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         yield AssociationField::new('gang')
-            ->setColumns(3)
+            ->setColumns(4)
             ->setFormTypeOptions([
                 'query_builder' => function (EntityRepository $er) {
                     return $er->createQueryBuilder('g')
@@ -33,15 +42,69 @@ class TerritoriesCrudController extends AbstractCrudController
             ->formatValue(static function (TerritoriesEnum $territories): string {
                 return $territories->enumToString();
             })
-            ->setColumns(3);
+            ->setColumns(4);
         if ($pageName !== Crud::PAGE_NEW) {
             yield TextField::new('incomeAsString')
-                ->setLabel('income')
+                ->setLabel('Income')
+                ->setColumns(4)
+                ->setFormTypeOption('disabled','disabled');
+            yield TextField::new('effect')
                 ->setColumns(6)
                 ->setFormTypeOption('disabled','disabled');
         }
-        yield TextField::new('effect')
-            ->setColumns(6)
-        ;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
+                return $action->setIcon('fa fa-eye');
+            })
+            ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+                $security = $this->security;
+                return $action
+                    ->setIcon('fa fa-pen-to-square')
+                    ->displayIf(static function ($entity) use ($security) {
+                        return self::checkTerritoriesOfCurrentUser($entity, $security);
+                    });
+            })
+            ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+                $security = $this->security;
+                return $action
+                    ->setIcon('fa fa-trash')
+                    ->displayIf(static function ($entity) use ($security) {
+                        return self::checkTerritoriesOfCurrentUser($entity, $security);
+                    });
+            })
+            ->update(Crud::PAGE_DETAIL, Action::EDIT, function (Action $action) {
+                $security = $this->security;
+                return $action
+                    ->setIcon('fa fa-file-alt')
+                    ->displayIf(static function ($entity) use ($security) {
+                        return self::checkTerritoriesOfCurrentUser($entity, $security);
+                    });
+            })
+            ->update(Crud::PAGE_DETAIL, Action::DELETE, function (Action $action) {
+                $security = $this->security;
+                return $action
+                    ->setIcon('fa fa-user')
+                    ->displayIf(static function ($entity) use ($security) {
+                        return self::checkTerritoriesOfCurrentUser($entity, $security);
+                    });
+            })
+            ;
+    }
+
+    public static function checkTerritoriesOfCurrentUser(Territories $territories, $security): bool
+    {
+        if(
+            $territories->getGang()->getUser() == $security->getUser()
+            || $security->isGranted('ROLE_ADMIN')
+        ) {
+            return true;
+        }
+
+        return false;
     }
 }
