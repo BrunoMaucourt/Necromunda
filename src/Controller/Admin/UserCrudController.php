e0c0d99d304eca6313ebfcabe4d6c191843d5316
Bruno <mct.bruno@gmail.com>
2024-05-29 19:05:44 +0200

    User CRUD controller - add security

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,14 +3,23 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class UserCrudController extends AbstractCrudController
 {
+    private Security $security;
+
+    public function __construct(Security $security){
+        $this->security = $security;
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -20,13 +29,60 @@ class UserCrudController extends AbstractCrudController
     {
         yield TextField::new('username')
             ->setColumns(6);
-        yield CollectionField::new('gang')
+        yield AssociationField::new('gang')
             ->setColumns(6);
         if($this->isGranted('ROLE_ADMIN')){
             yield ArrayField::new('roles')
                 ->setColumns(6);
         }
-        yield TextField::new('password')
-            ->hideOnIndex();
+
+        $entityInstance = $this->getContext()->getEntity()->getInstance();
+
+        if ($entityInstance == $this->security->getUser() || $this->security->isGranted('ROLE_ADMIN')) {
+            yield TextField::new('password')
+                ->hideOnIndex();
+        }
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
+                return $action->setIcon('fa fa-eye');
+            })
+            ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+                $security = $this->security;
+                return $action
+                    ->setIcon('fa fa-pen-to-square')
+                    ->displayIf(static function ($entity) use ($security) {
+                        return $entity == $security->getUser() || $security->isGranted('ROLE_ADMIN');
+                    });
+            })
+            ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+                $security = $this->security;
+                return $action
+                    ->setIcon('fa fa-trash')
+                    ->displayIf(static function ($entity) use ($security) {
+                        return $entity == $security->getUser() || $security->isGranted('ROLE_ADMIN');
+                    });
+            })
+            ->update(Crud::PAGE_DETAIL, Action::EDIT, function (Action $action) {
+                $security = $this->security;
+                return $action
+                    ->setIcon('fa fa-file-alt')
+                    ->displayIf(static function ($entity) use ($security) {
+                        return $entity == $security->getUser() || $security->isGranted('ROLE_ADMIN');
+                    });
+            })
+            ->update(Crud::PAGE_DETAIL, Action::DELETE, function (Action $action) {
+                $security = $this->security;
+                return $action
+                    ->setIcon('fa fa-user')
+                    ->displayIf(static function ($entity) use ($security) {
+                        return $entity == $security->getUser() || $security->isGranted('ROLE_ADMIN');
+                    });
+            })
+        ;
     }
 }
