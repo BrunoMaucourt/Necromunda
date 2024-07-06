66ff6f9056db3653cb3a0e4adf0085dd2fa96e50
Bruno <mct.bruno@gmail.com>
2024-07-06 21:38:56 +0200

    Game CRUD - add CRUD controller

--- src/Controller/Admin/GamesCrudController.php
+++ src/Controller/Admin/GamesCrudController.php
@@ -17,16 +17,237 @@ class GamesCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        if (
+            $pageName === Crud::PAGE_NEW
+        ) {
+            $session = $this->requestStack->getSession();
+            $gang1Id = $session->get('gang1');
+            $gang2Id = $session->get('gang2');
+        } elseif (
+            $pageName === Crud::PAGE_EDIT ||
+            $pageName === Crud::PAGE_DETAIL
+        ) {
+            $game = $this->getContext()->getEntity()->getInstance();
+
+            if ($game instanceof Game) {
+                $gang1Id = $game->getGang1() ? $game->getGang1()->getId() : null;
+                $gang2Id = $game->getGang2() ? $game->getGang2()->getId() : null;
+            }
+        } else {
+            $gang1Id = null;
+            $gang2Id = null;
+        }
+
+        $gangs = [$gang1Id, $gang2Id];
+
+        yield ScenarioField::new('scenario', 'Scenario')
+            ->hideWhenCreating()
+            ->formatValue(static function (ScenariosEnum $scenariosEnum): string {
+                return $scenariosEnum->enumToString();
+            })
+            ->setColumns(6);
+        yield DateField::new('date')
+            ->setColumns(6);
+        yield FormField::addPanel('Players')
+            ->hideWhenCreating()
+            ->setIcon('fa fa-users')
+            ->collapsible();
+        yield AssociationField::new('gang1')
+            ->hideWhenCreating()
+            ->setColumns(6)
+            ->setFormTypeOption('disabled','disabled')
+            ->setFormTypeOptions([
+                'query_builder' => function (EntityRepository $er) use ($gang1Id) {
+                    return $er->createQueryBuilder('g')
+                        ->where('g.id = :id')
+                        ->setParameter('id', $gang1Id);
+                },
+            ]);
+        yield AssociationField::new('gang2')
+            ->hideWhenCreating()
+            ->setColumns(6)
+            ->setFormTypeOption('disabled','disabled')
+            ->setFormTypeOptions([
+                'query_builder' => function (EntityRepository $er) use ($gang2Id) {
+                    return $er->createQueryBuilder('g')
+                        ->where('g.id = :id')
+                        ->setParameter('id', $gang2Id);
+                },
+            ]);
+//        yield FormField::addPanel('Gangers involved in game')
+//            ->hideWhenCreating()
+//            ->setIcon('fa fa-user-secret')
+//            ->collapsible();
+//        yield AssociationField::new('gangers')
+//            ->hideWhenCreating()
+//            ->hideOnIndex()
+//            ->setLabel('Ganger 1')
+//            ->setColumns(6)
+//            ->setFormTypeOptions([
+//                'query_builder' => function (EntityRepository $er) use ($gang1Id) {
+//                    return $er->createQueryBuilder('g')
+//                        ->where('g.gang1 = :gang')
+//                        ->setParameter('gang', $gang1Id);
+//                },
+//            ]);
+//        yield AssociationField::new('gangers')
+//            ->hideWhenCreating()
+//            ->hideOnIndex()
+//            ->setLabel('Ganger 2')
+//            ->setColumns(6)
+//            ->setFormTypeOptions([
+//                'query_builder' => function (EntityRepository $er) use ($gang2Id) {
+//                    return $er->createQueryBuilder('g')
+//                        ->where('g.gang2 = :gang')
+//                        ->setParameter('gang', $gang2Id);
+//                },
+//            ]);
+        yield FormField::addPanel('Injuries during game')
+            ->hideWhenCreating()
+            ->setIcon('fa fa-user-injured')
+            ->collapsible();
+        yield AssociationField::new('injuries')
+            ->setLabel('Gang 1')
+            ->hideWhenCreating()
+            ->hideOnIndex()
+            ->setColumns(6)->setColumns(6)->setFormTypeOptions([
+                'query_builder' => function (EntityRepository $er) use ($gang1Id) {
+                    return $er->createQueryBuilder('g')
+                        ->where('g.id = :id')
+                        ->setParameter('id', $gang1Id);
+                },
+            ]);
+        yield AssociationField::new('injuries')
+            ->setLabel('Gang 2')
+            ->hideWhenCreating()
+            ->hideOnIndex()
+            ->setColumns(6)->setColumns(6)->setFormTypeOptions([
+                'query_builder' => function (EntityRepository $er) use ($gang2Id) {
+                    return $er->createQueryBuilder('g')
+                        ->where('g.id = :id')
+                        ->setParameter('id', $gang2Id);
+                },
+            ]);
+//        yield FormField::addPanel('Territories exploited by gangers')
+//            ->setIcon('fa fa-warehouse')
+//            ->hideWhenCreating()
+//            ->collapsible();
+//        yield AssociationField::new('territories')
+//            ->setColumns(6)
+//            ->hideWhenCreating()
+//            ->setFormTypeOptions([
+//                'query_builder' => function (EntityRepository $er) use ($gang1Id) {
+//                    return $er->createQueryBuilder('g')
+//                        ->where('g.gang = :gang')
+//                        ->setParameter('gang', $gang1Id);
+//                },
+//            ]);
+//        yield AssociationField::new('territories')
+//            ->setColumns(6)
+//            ->hideWhenCreating()
+//            ->setFormTypeOptions([
+//                'query_builder' => function (EntityRepository $er) use ($gang2Id) {
+//                    return $er->createQueryBuilder('g')
+//                        ->where('g.gang = :gang')
+//                        ->setParameter('gang', $gang2Id);
+//                },
+//            ]);
+        yield FormField::addPanel('Score')
+            ->hideWhenCreating()
+            ->setIcon('fa fa-list-ol')
+            ->collapsible();
+        yield IntegerField::new('getGang1RatingBeforeGame')
+            ->hideOnIndex()
+            ->hideWhenCreating()
+            ->setColumns(6)
+            ->hideOnIndex();
+        yield IntegerField::new('getGang1RatingAfterGame')
+            ->hideWhenCreating()
+            ->setLabel('Gang 1 rating after game')
+            ->setColumns(6);
+        yield IntegerField::new('getGang2RatingBeforeGame')
+            ->hideOnIndex()
+            ->hideWhenCreating()
+            ->setColumns(6);
+        yield IntegerField::new('getGang2RatingAfterGame')
+            ->hideWhenCreating()
+            ->setLabel('Gang 2 rating after game')
+            ->setColumns(6);
+        yield FormField::addPanel('Credits and loots')
+            ->hideWhenCreating()
+            ->setIcon('fa fa-gem')
+            ->collapsible();
+        yield NumberField::new('gang1creditsBeforeGame')
+            ->hideOnIndex()
+            ->hideWhenCreating()
+            ->setColumns(6)
+            ->hideOnIndex();
+        yield NumberField::new('gang2creditsBeforeGame')
+            ->hideOnIndex()
+            ->hideWhenCreating()
+            ->setColumns(6)
+            ->hideOnIndex();
+        yield NumberField::new('gang1creditsAfterGame')
+            ->hideWhenCreating()
+            ->setColumns(6);
+        yield NumberField::new('gang2creditsAfterGame')
+            ->hideWhenCreating()
+            ->setColumns(6);
+//        yield AssociationField::new('loots')
+//            ->hideWhenCreating()
+//            ->hideOnIndex()
+//            ->setColumns(6)
+//            ->hideOnIndex()
+//            ->setFormTypeOptions([
+//                'query_builder' => function (EntityRepository $er) use ($gang1Id) {
+//                    return $er->createQueryBuilder('g')
+//                        ->where('g.gang = :gang')
+//                        ->setParameter('gang', $gang1Id);
+//                },
+//            ]);
+//        yield AssociationField::new('loots')
+//            ->hideWhenCreating()
+//            ->hideOnIndex()
+//            ->setColumns(6)
+//            ->hideOnIndex()
+//            ->setFormTypeOptions([
+//                'query_builder' => function (EntityRepository $er) use ($gang2Id) {
+//                    return $er->createQueryBuilder('g')
+//                        ->where('g.gang = :gang')
+//                        ->setParameter('gang', $gang2Id);
+//                },
+//            ]);
+        yield FormField::addPanel('Winner')
+            ->setIcon('fa fa-trophy')
+            ->collapsible();
+        yield AssociationField::new('winner')
+            ->setColumns(6)
+            ->setFormTypeOptions([
+                'query_builder' => function (EntityRepository $er) use ($gangs) {
+                    return $er->createQueryBuilder('g')
+                        ->where('g.id IN (:ids)')
+                        ->setParameter('ids', $gangs);
+                },
+            ]);
+        yield FormField::addPanel('Game background and details')
+            ->setIcon('fa fa-book')
+            ->collapsible();
+        yield TextareaField::new('background')
+            ->setColumns(12)
+            ->hideOnIndex();
+        yield TextareaField::new('summary')
+            ->setColumns(12)
+            ->hideWhenCreating()
+            ->hideOnIndex()
+            ->renderAsHtml();
     }
     public function configureActions(Actions $actions): Actions
     {
+        $urlForCustomNewGameAction = $this->adminUrlGenerator
+            ->setRoute('choose_gangs')
+            ->generateUrl();
         $customNewGameAction = Action::new('newGame', 'Add game')
-            ->linkToUrl('/admin/choose-gangs')
+            ->linkToUrl($urlForCustomNewGameAction)
             ->setCssClass('btn btn-primary')
             ->createAsGlobalAction();
 
