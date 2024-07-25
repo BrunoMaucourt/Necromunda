3d2be4f190aae42dd0e3ccf492c15645d1e89efa
Bruno <mct.bruno@gmail.com>
2024-07-25 20:40:56 +0200

    Weapon - form to select ganger before to add weapon (or equipment)

---
+++ src/Form/selectGangerForItems.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Ganger;
+
+class selectGangerForItems extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        /** @var Ganger $gangers */
+        $gangers = $options['gangers'];
+
+        $choices = [];
+        foreach ($gangers as $ganger) {
+            $choices[$ganger->getName() . ' - ' . $ganger->getType()->enumToString()] = $ganger->getId();
+        }
+
+        $builder->add('ganger', ChoiceType::class, [
+            'choices' => $choices,
+            'label' => 'Select Ganger',
+            'expanded' => true,
+            'multiple' => false,
+            'attr' => [
+                'class' => 'mt-2 mb-3',
+            ],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => null,
+        ]);
+
+        $resolver->setRequired(['gangers']);
+        $resolver->setAllowedTypes('gangers', 'array');
+    }
+}
