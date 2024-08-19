e8c017da7953fa11c3ed34e379c6f8c49f7b219a
Bruno <mct.bruno@gmail.com>
2024-08-19 21:00:47 +0200

    Loot - update CRUD fields

--- src/Controller/Admin/LootCrudController.php
+++ src/Controller/Admin/LootCrudController.php
@@ -4,8 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Loot;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class LootCrudController extends AbstractCrudController
@@ -15,14 +15,11 @@ class LootCrudController extends AbstractCrudController
         return Loot::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('name');
+        yield IntegerField::new('cost');
+        yield AssociationField::new('gang');
+        yield AssociationField::new('game');
     }
-    */
 }
