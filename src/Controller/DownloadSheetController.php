0b9a83c9d3ef6915716906b2a47ef23ca3f0f196
Bruno <mct.bruno@gmail.com>
2024-08-25 11:54:03 +0200

    Gang sheet - add more data to gang sheet

--- src/Controller/DownloadSheetController.php
+++ src/Controller/DownloadSheetController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Equipement;
 use App\Entity\Gang;
 use App\Entity\Ganger;
+use App\Entity\Injury;
+use App\Entity\Skill;
 use App\Entity\Weapon;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,14 +26,75 @@ class DownloadSheetController extends AbstractController
     #[Route('/admin/generateGangTable/{id}', name: 'generate_gang_table')]
     public function generateGangTable(int $id): Response
     {
+        $equipmentRepo = $this->entityManager->getRepository(Equipement::class);
         $gangRepo = $this->entityManager->getRepository(Gang::class);
         $gangerRepo = $this->entityManager->getRepository(Ganger::class);
+        $injuryRepo = $this->entityManager->getRepository(Injury::class);
+        $skillRepo = $this->entityManager->getRepository(Skill::class);
+        $weaponRepo = $this->entityManager->getRepository(Weapon::class);
 
+        $equipments = $equipmentRepo->findAllEquipmentsByGang($id);
         $gang = $gangRepo->find($id);
         $gangers = $gangerRepo->findAliveByGang($id);
+        $injuries = $injuryRepo->findAllInjuriesByGang($id);
+        $skills = $skillRepo->findAllSkillsByGang($id);
+        $weapons = $weaponRepo->findAllWeaponsTypeByGang($id);
+
+        $referenceValues = [
+            'leader' => [
+                'move' => 4,
+                'weaponSkill' => 4,
+                'ballisticSkill' => 4,
+                'strength' => 3,
+                'toughness' => 3,
+                'wounds' => 1,
+                'initiative' => 4,
+                'attacks' => 1,
+                'leadership' => 8,
+            ],
+            'heavy' => [
+                'move' => 4,
+                'weaponSkill' => 3,
+                'ballisticSkill' => 3,
+                'strength' => 3,
+                'toughness' => 3,
+                'wounds' => 1,
+                'initiative' => 3,
+                'attacks' => 1,
+                'leadership' => 7,
+            ],
+            'ganger' => [
+                'move' => 4,
+                'weaponSkill' => 3,
+                'ballisticSkill' => 3,
+                'strength' => 3,
+                'toughness' => 3,
+                'wounds' => 1,
+                'initiative' => 3,
+                'attacks' => 1,
+                'leadership' => 7,
+            ],
+            'juve' => [
+                'move' => 4,
+                'weaponSkill' => 2,
+                'ballisticSkill' => 2,
+                'strength' => 3,
+                'toughness' => 3,
+                'wounds' => 1,
+                'initiative' => 3,
+                'attacks' => 1,
+                'leadership' => 6,
+            ],
+        ];
 
         return $this->render('download/gangSheet.html.twig', [
+            'equipments' => $equipments,
             'gang' => $gang,
+            'gangers' => $gangers,
+            'injuries' => $injuries,
+            'referenceValues' => $referenceValues,
+            'skills' => $skills,
+            'weapons' => $weapons,
         ]);
     }
 }
