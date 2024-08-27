a7dd971ba3e83f85d8784affa5105f00127caae8
Bruno <mct.bruno@gmail.com>
2024-08-27 18:54:08 +0200

    Gangers WIP - add hired guns

--- src/Enum/GangerTypeEnum.php
+++ src/Enum/GangerTypeEnum.php
@@ -10,6 +10,12 @@ enum GangerTypeEnum: string
     case heavy = 'heavy';
     case ganger = 'ganger';
     case juve = 'juve';
+    case underhive_scum = 'underhive scum';
+    case bounty_hunter = 'bounty hunter';
+    case ratskin_scout = 'ratskin scout';
+
+    const GANG = 'gang';
+    const HIRED_GUNS = 'hired guns';
 
     public function enumToString(): string
     {
@@ -19,6 +25,23 @@ enum GangerTypeEnum: string
             self::heavy => GangerTypeEnum::heavy->value,
             self::ganger => GangerTypeEnum::ganger->value,
             self::juve =>  GangerTypeEnum::juve->value,
+            self::underhive_scum =>  GangerTypeEnum::underhive_scum->value,
+            self::bounty_hunter =>  GangerTypeEnum::bounty_hunter->value,
+            self::ratskin_scout =>  GangerTypeEnum::ratskin_scout->value,
+        };
+    }
+
+    public function getType(): string
+    {
+        return match($this)
+        {
+            self::leader => self::GANG,
+            self::heavy => self::GANG,
+            self::ganger => self::GANG,
+            self::juve => self::GANG,
+            self::underhive_scum => self::HIRED_GUNS,
+            self::bounty_hunter => self::HIRED_GUNS,
+            self::ratskin_scout => self::HIRED_GUNS,
         };
     }
 }
