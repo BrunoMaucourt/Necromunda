c3a3b21b21ce1b8f13e02c98423c4a29c6ce954e
Bruno <mct.bruno@gmail.com>
2024-08-28 20:13:27 +0200

    Gangers WIP - add advancements for underhive scum

--- src/EventListener/GangerListener.php
+++ src/EventListener/GangerListener.php
@@ -3,8 +3,10 @@
 namespace App\EventListener;
 
 use App\Entity\Ganger;
+use App\Entity\Skill;
 use App\Entity\Weapon;
 use App\Enum\GangerTypeEnum;
+use App\Enum\SkillsEnum;
 use App\Enum\WeaponsEnum;
 use App\Exception\GangerVerificationFailedException;
 use App\service\HistoryService;
@@ -150,9 +152,139 @@ class GangerListener
 
             $object->addWeapon($freeKnife);
 
+            $hiredGunMessage = '';
+            $oldDicesResultsHiredGun = [];
+            if ($ganger->getType() === GangerTypeEnum::underhive_scum) {
+                while ($advancements > 0) {
+
+                    $dice1 = mt_rand(1, 6);
+                    $dicesResults = $dice1;
+                    if ($dice1 !== 1 ) {
+                        $dice2 = mt_rand(1, 6);
+                        $dicesResults .= $dice2;
+                        if ($dice1 > 3 && $dice2 > 3) {
+                            $dice3 = mt_rand(1, 6);
+                            $dicesResults .= $dice3;
+                        }
+                    }
+
+                    // Check to avoid have 2 time the same skill
+                    if (
+                        in_array($dicesResults, $oldDicesResultsHiredGun) &&
+                        $dicesResults > 440
+                    ) {
+                        continue;
+                    }
+                    $oldDicesResultsHiredGun[] = $dicesResults;
+
+                    switch ($dicesResults) {
+                        case 1:
+                            $ganger->setBallisticSkill($ganger->getBallisticSkill() + 1);
+                            $dicesResultsMessage = "+1 Bs";
+                            break;
+                        case 21: case 22: case 23:
+                            $ganger->setInitiative($ganger->getInitiative() + 1);
+                            $dicesResultsMessage = "+1 I";
+                            break;
+                        case 24: case 25: case 26: case 36:
+                            $ganger->setLeadership($ganger->getLeadership() + 1);
+                            $dicesResultsMessage = "+1 Le";
+                            break;
+                        case 31:
+                            $ganger->setWeaponSkill($ganger->getWeaponSkill() + 1);
+                            $dicesResultsMessage = "+1 Ws";
+                            break;
+                        case 32:
+                            $ganger->setStrength($ganger->getStrength() + 1);
+                            $dicesResultsMessage = "+1 S";
+                            break;
+                        case 33:
+                            $ganger->setToughness($ganger->getToughness() + 1);
+                            $dicesResultsMessage = "+1 T";
+                            break;
+                        case 34:
+                            $ganger->setWounds($ganger->getWounds() + 1);
+                            $dicesResultsMessage = "+1 W";
+                            break;
+                        case 35:
+                            $ganger->setAttacks($ganger->getAttacks() + 1);
+                            $dicesResultsMessage = "+1 A";
+                            break;
+                        case 41: case 42: case 51: case 52: case 61: case 62:
+                            $gunfighter = new Skill();
+                            $gunfighter->setName(SkillsEnum::ShootingGunfighter);
+                            $ganger->addSkill($gunfighter);
+                            $dicesResultsMessage = "skill - gun figther (Shooting)";
+                            break;
+                        case 43: case 53: case 63:
+                            $quickWitted = new Skill();
+                            $quickWitted->setName(SkillsEnum::AgilityQuickWitted);
+                            $ganger->addSkill($quickWitted);
+                            $dicesResultsMessage = "skill - quick witted (Agility)";
+                            break;
+                        case 441: case 451: case 461: case 541: case 551: case 561: case 641: case 651: case 661:
+                            $crackShot = new Skill();
+                            $crackShot->setName(SkillsEnum::ShootingCrackShot);
+                            $ganger->addSkill($crackShot);
+                            $dicesResultsMessage = "skill - crack shot (Shooting)";
+                            break;
+                        case 442: case 452: case 462: case 542: case 552: case 562: case 642: case 652: case 662:
+                            $fastShot = new Skill();
+                            $fastShot->setName(SkillsEnum::ShootingFastShot);
+                            $ganger->addSkill($fastShot);
+                            $dicesResultsMessage = "skill - fast shot (Shooting)";
+                            break;
+                        case 443: case 453: case 463: case 543: case 553: case 563: case 643: case 653: case 663:
+                            $hipShooting = new Skill();
+                            $hipShooting->setName(SkillsEnum::ShootingHipShooting);
+                            $ganger->addSkill($hipShooting);
+                            $dicesResultsMessage = "skill - hip shooting (Shooting)";
+                            break;
+                        case 444: case 454: case 464: case 544: case 554: case 564: case 644: case 654: case 664:
+                            $dodge = new Skill();
+                            $dodge->setName(SkillsEnum::AgilityDodge);
+                            $ganger->addSkill($dodge);
+                            $dicesResultsMessage = "skill - dodge (Agility)";
+                            break;
+                        case 445: case 455: case 465: case 545: case 555: case 565: case 645: case 655: case 665:
+                            $rapidFire = new Skill();
+                            $rapidFire->setName(SkillsEnum::ShootingRapidFire);
+                            $ganger->addSkill($rapidFire);
+                            $dicesResultsMessage = "skill - rapid fire (Shooting)";
+                            break;
+                        case 446: case 456: case 466: case 546: case 556: case 566: case 646: case 656: case 666:
+                            $killerReputation = new Skill();
+                            $killerReputation->setName(SkillsEnum::FerocityKillerReputation);
+                            $ganger->addSkill($killerReputation);
+                            $dicesResultsMessage = "skill - killer reputation (Ferocity)";
+                            break;
+                    }
+
+                    $hiredGunMessage .= "advancement dice result - " . $dicesResults . " : " .$dicesResultsMessage . "\n";
+                    $advancements--;
+                }
+            }
+
+            if ($ganger->getType() === GangerTypeEnum::bounty_hunter) {
+                while ($advancements > 0) {
+
+                    $advancements--;
+                }
+            }
+
+            if ($ganger->getType() === GangerTypeEnum::ratskin_scout) {
+                while ($advancements > 0) {
+
+                    $advancements--;
+                }
+            }
+
+            $flashMessage = date("Y/m/d") . ' - ' . "New ganger : " . $ganger->getName() . " (". $ganger->getType()->enumToString() .") : \n \n" . '- experience = '. $experienceMessage ." \n- free knife\n" . $hiredGunMessage;
+            $ganger->setHistory( $flashMessage);
+
             $flash->add(
                 'success',
-                'New ganger : ' . $ganger->getName() . ' ('. $ganger->getType()->enumToString() .') :<br><br>' . '- experience = '. $experienceMessage .'<br>- free knife<br>'
+                "New ganger : " . $ganger->getName() . " (". $ganger->getType()->enumToString() .") : <br> <br>" . '- experience = '. $experienceMessage ." <br>- free knife<br>" . $hiredGunMessage
             );
         }
     }
