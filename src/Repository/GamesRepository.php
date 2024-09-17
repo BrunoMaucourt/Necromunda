a53cdfab95fc4741223bc91aef8a4544ce0d5ac6
Bruno <mct.bruno@gmail.com>
2024-09-17 19:03:22 +0200

    Game chart : get data for all games

--- src/Repository/GamesRepository.php
+++ src/Repository/GamesRepository.php
@@ -15,4 +15,15 @@ class GamesRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Game::class);
     }
+
+    public function getGangRatingsPerDate(): array
+    {
+        $qb = $this->createQueryBuilder('g')
+            ->select('g.date, gang1.name AS gang1_name, g.gang1RatingAfterGame AS gang1_rating_after, g.gang1RatingBeforeGame AS gang1_rating_before, gang2.name AS gang2_name, g.gang2RatingBeforeGame AS gang2_rating_before, g.gang2RatingAfterGame AS gang2_rating_after')
+            ->leftJoin('g.gang1', 'gang1')
+            ->leftJoin('g.gang2', 'gang2')
+            ->orderBy('g.date', 'ASC');
+
+        return $qb->getQuery()->getResult();
+    }
 }
