151eb0544fc8a965f3ed4dbd3ca0dd8475e1eed8
Bruno <mct.bruno@gmail.com>
2024-09-30 21:17:11 +0200

    Information list - move code from weapon controller

---
+++ src/Controller/InformationsListsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Enum\EquipementsEnum;
+use App\Enum\SpecialWeaponEnum;
+use App\Enum\WeaponsEnum;
+use App\service\WeaponService;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InformationsListsController extends AbstractController
+{
+    #[Route('/{_locale<%app.supported_locales%>}/admin/weaponInformations', name: 'weapon_informations')]
+    public function WeaponInformations(Request $request): Response
+    {
+        $allWeapons = WeaponsEnum::cases();
+        $allSpecialWeapons = SpecialWeaponEnum::cases();
+
+        foreach ($allWeapons as $key => $weapon) {
+            if (
+                $weapon == WeaponsEnum::SHOTGUN ||
+                $weapon == WeaponsEnum::COMBAT_RIFLE ||
+                $weapon == WeaponsEnum::PLASMA_GUN ||
+                $weapon == WeaponsEnum::PLASMA_PISTOL ||
+                $weapon == WeaponsEnum::MISSILE_LAUNCHER
+            ) {
+                unset($allWeapons[$key]);
+            }
+        }
+
+        return $this->render('informations/weapon.html.twig', [
+            'specials' => $allSpecialWeapons,
+            'weapons' => $allWeapons
+        ]);
+    }
+}
