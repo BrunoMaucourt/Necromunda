9e962636a7c458a174b2c8a5f65a9c6e94e678bc
Bruno <mct.bruno@gmail.com>
2024-10-09 19:50:40 +0200

    Weapon listener - cleaning code

--- src/EventListener/WeaponListener.php
+++ src/EventListener/WeaponListener.php
@@ -5,31 +5,19 @@ namespace App\EventListener;
 use App\Entity\Weapon;
 use App\service\WeaponService;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Event\PrePersistEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 #[AsDoctrineListener(event: Events::prePersist, priority: 500, connection: 'default')]
 #[AsDoctrineListener(event: Events::preUpdate, priority: 500, connection: 'default')]
 class WeaponListener
 {
-    private AdminUrlGenerator $adminUrlGenerator;
-
-    private EntityManagerInterface $entityManager;
-
     private WeaponService $weaponService;
 
     public function __construct (
-        AdminUrlGenerator $adminUrlGenerator,
-        EntityManagerInterface $entityManager,
         WeaponService $weaponService
-    )
-    {
-        $this->adminUrlGenerator = $adminUrlGenerator;
-        $this->entityManager = $entityManager;
+    ) {
         $this->weaponService = $weaponService;
     }
 
