366c92265ee839acbdb4ef2ea6b0d5656742038d
Bruno <mct.bruno@gmail.com>
2024-12-10 19:53:08 +0100

    Loot - activate loot in CRUD only for admin

--- src/Controller/Admin/LootCrudController.php
+++ src/Controller/Admin/LootCrudController.php
@@ -3,20 +3,27 @@
 namespace App\Controller\Admin;
 
 use App\EasyAdmin\LootField;
+use App\Entity\Gang;
 use App\Entity\Loot;
 use App\Enum\LootEnum;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class LootCrudController extends AbstractCrudController
 {
+    private Security $security;
+
     private TranslatorInterface $translator;
 
     public function __construct(
+        Security $security,
         TranslatorInterface $translator
     ){
+        $this->security = $security;
         $this->translator = $translator;
     }
 
@@ -34,5 +41,9 @@ class LootCrudController extends AbstractCrudController
         yield IntegerField::new('cost', $this->translator->trans('cost'));
         yield AssociationField::new('gang', $this->translator->trans('gang'));
         yield AssociationField::new('game', $this->translator->trans('game'));
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            yield BooleanField::new('active', $this->translator->trans('active'));
+        }
     }
 }
