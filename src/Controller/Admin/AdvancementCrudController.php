6c4be77eff5a3104ed5048e64ec70ffb85ce4717
Bruno <mct.bruno@gmail.com>
2025-01-04 18:32:18 +0100

    Advancement - update CRUD

--- src/Controller/Admin/AdvancementCrudController.php
+++ src/Controller/Admin/AdvancementCrudController.php
@@ -4,22 +4,33 @@ namespace App\Controller\Admin;
 
 use App\Entity\Advancement;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AdvancementCrudController extends AbstractCrudController
 {
+    private TranslatorInterface $translator;
+
+    public function __construct(
+        TranslatorInterface $translator,
+    ) {
+        $this->translator = $translator;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Advancement::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield AssociationField::new('ganger', $this->translator->trans('Ganger'));
+
+        yield TextField::new('content', $this->translator->trans('Content'));
+
+        yield AssociationField::new('game', $this->translator->trans('Game'));
+
+        yield AssociationField::new('skill', $this->translator->trans('Skill'));
     }
-    */
 }
